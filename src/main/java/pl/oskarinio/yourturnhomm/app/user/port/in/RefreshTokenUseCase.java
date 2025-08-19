c4a1f6a88ebfeea007c7c99f02cc5e5d4f26d2a9
oSkarinio143 <oskarz143@onet.pl>
2025-08-19 08:46:10 +0200

    Minor changes v4

--- src/main/java/pl/oskarinio/yourturnhomm/app/user/port/in/RefreshTokenUseCase.java
+++
@@ -1,9 +0,0 @@
-package pl.oskarinio.yourturnhomm.app.user.port.in;
-
-import org.springframework.data.repository.query.Param;
-
-import java.time.Instant;
-
-public interface RefreshTokenUseCase {
-    void deleteExpiredToken(Instant date);
-}
--- src/main/java/pl/oskarinio/yourturnhomm/app/user/port/out/RefreshTokenRepositoryPort.java
+++ src/main/java/pl/oskarinio/yourturnhomm/app/user/port/out/RefreshTokenRepositoryPort.java
@@ -1,13 +1,7 @@
 package pl.oskarinio.yourturnhomm.app.user.port.out;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import pl.oskarinio.yourturnhomm.domain.model.entity.RefreshTokenEntity;
-
 import java.time.Instant;
 
-public interface RefreshTokenRepositoryPort extends JpaRepository<RefreshTokenEntity, Integer> {
+public interface RefreshTokenRepositoryPort{
     void deleteExpiredToken(Instant date);
 }
